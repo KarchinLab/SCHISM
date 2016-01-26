996d81f24a93515aeedf9ce9817fe9b887f91099
Noushin Niknafs <Noushin@Noushins-MBP-2.fios-router.home>
2016-01-26 18:51:29 -0500

    fixing setup.py install requirements

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ setup(
     url='http://www.karchinlab.org/apps/appSchism.html',
     license='LICENSE.txt',
     description='SCHISM: Subclonal Hierarchy Inference from Somatic Mutations',
-    long_description='\n' + open('README.txt').read(),
-    install_requires=["numpy >= 1.7.1",\
-                      "scipy >= 0.12.0",\
-                      "PyYAML >= 3.11",\
-                      "matplotlib >= 1.2.0",\
-                      "sklearn >= 0.14.0"], 
+    long_description='\n' + open('README.txt').read()
 )
 
+#     install_requires=["numpy >= 1.7.1",\
+#                      "scipy >= 0.12.0",\
+#                      "PyYAML >= 3.11",\
+#                      "matplotlib >= 1.2.0",\
+#                      "sklearn >= 0.14.0"], 
