f8ef8786ebc063aa92d9be496c12729f087b5d8e
Noushin Niknafs <Noushin@Noushins-MacBook-Pro-2.local>
2016-02-08 09:04:41 -0500

    improved error checking in config parsing module

--- SCHISM/utils.py
+++ SCHISM/utils.py
@@ -1,3 +1,4 @@
+import sys
 import yaml
 
 class Config():
@@ -7,4 +8,10 @@ class Config():
         config = yaml.load(f_h)
         f_h.close()
         self.__dict__.update(config)
+        # check parameter set for validity
+        if hasattr(self, 'cluster_analysis') and self.cluster_analysis == 'schism':
+            if self.hypothesis_test['test_level'] == 'clusters':
+                print >>sys.stderr, 'Cluster analysis by SCHISM requires setting the "test_level" parameter to "mutations".'
+                print >>sys.stderr, 'Please update the configuration file before proceeding.'
+                sys.exit()
         return
