46a041274649ea3207ce35e80e9ad9b272d1384b
Noushin Niknafs <Niknafs@users.noreply.github.com>
2016-02-19 11:30:45 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='SCHISM',
-    version='1.1.0',
+    version='1.1.1',
     author='Noushin Niknafs',
     author_email='niknafs@jhu.edu',
     packages=['SCHISM'],
